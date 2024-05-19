46b491f0526748e1846ac8c39a20101b3a447831
Alexander Stucker <alexander.stucker@hftm.ch>
2024-05-19 23:42:46 +0200

    Created a NavBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:blog_app/nav_bar.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
+//Root of the App. Needs to be a Stateless Widget
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+//Statefull Widget, because the entries of the Blogs are going to be different
+// Maybe needs to be its own class-file in the Future
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+// Stateclass of MyHomepage
+class MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold for now
+    return Scaffold(
+      // drawer with a Navbar
+      drawer: NavBar(),
+      /////////// TBD FROM HERE //////////////
+      appBar: AppBar(
+        title: Text("Blog List"),
       ),
+      body: Center(),
     );
   }
 }
---
+++ lib/nav_bar.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'nav_bar_list_tile.dart'; // Import the NavListTile widget
+
+class NavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          // Using DrawerHeader for more flexibility in alignment since Avatar can not be aligned in UserAccountsDrawerHeader.
+          // This might cause issues on different screen sizes due to fixed height and content arrangement.
+          DrawerHeader(
+            decoration: const BoxDecoration(
+              color: Colors.blue,
+              image: DecorationImage(
+                image: NetworkImage(
+                  'https://images.unsplash.com/photo-1432821596592-e2c18b78144f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+            padding: EdgeInsets.zero,
+            child: Container(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 45,
+                    child: ClipOval(
+                      child: Image.network(
+                        "https://images.unsplash.com/photo-1545996124-0501ebae84d0?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                        width: 90,
+                        height: 90,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    "AccountName",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Text(
+                    "AccountEmail",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          // Navigation Tiles its  own Class-File
+          NavListTile(
+            icon: Icons.home,
+            title: "Home",
+            onTap: () => null,
+          ),
+          Divider(),
+          NavListTile(
+            icon: Icons.add_box_outlined,
+            title: "Write new Blog",
+            onTap: () => null,
+          ),
+          Divider(),
+          Expanded(
+            child: Container(),
+          ),
+          Divider(),
+          NavListTile(
+            icon: Icons.logout,
+            title: "Logout",
+            onTap: () => null,
+          ),
+        ],
+      ),
+    );
+  }
+}
